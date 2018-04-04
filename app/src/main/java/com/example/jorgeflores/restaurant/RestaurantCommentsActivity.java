b80c80f0285deb02afec1268e4805bb479e11714
Jorge Flores <negoxp@gmail.com>
2018-04-03 23:55:34 -0700

    I changed some layout and i design and add some functionality in the commentpage. all i want u to store the value on button click in the database. beacuse i dont know what are the fields in the databse.

--- app/src/main/java/com/example/jorgeflores/restaurant/RestaurantCommentsActivity.java
+++ app/src/main/java/com/example/jorgeflores/restaurant/RestaurantCommentsActivity.java
@@ -1,14 +1,41 @@
 package com.example.jorgeflores.restaurant;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RestaurantCommentsActivity extends AppCompatActivity {
+    RatingBar ratingBar;
+    private FirebaseDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_comments);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ratingBar=(RatingBar)findViewById(R.id.ratingBarID);
+        Button btnSubmit=(Button)findViewById(R.id.btnReview);
+        EditText getReview=(EditText)findViewById(R.id.getReview);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                float getRating = ratingBar.getRating();
+                //Toast.makeText(RestaurantCommentsActivity.this,getRating+"",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
